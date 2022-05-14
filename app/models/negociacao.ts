d286e4b36c2d7c6568fa4b4e33ff5f4fb67ae7c1
Eduardo Moraes Goncalves <eduardo.m.goncalves@unicuritiba.com.br>
2022-05-14 19:22:06 -0300

    Simplificando o código.

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,27 +1,53 @@
 export class Negociacao {
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
+    // private _data: Date;
+    // private _quantidade: number;
+    // private _valor: number;
 
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+    // constructor(data: Date, quantidade: number, valor: number) {
+    //     this._data = data;
+    //     this._quantidade = quantidade;
+    //     this._valor = valor;
+    // }
+
+    // --------------------------------------------------//
+
+    // O código acima pode ser escrito (em TypeScript) da seguinte forma:
+    // constructor(
+    //     private _data: Date,
+    //     private _quantidade: number,
+    //     private _valor: number
+    // ) {}
+
+    // get data(): Date {
+    //     return this._data;
+    // }
+
+    // get quantidade(): number {
+    //     return this._quantidade;
+    // }
+
+    // get valor(): number {
+    //     return this._valor;
+    // }
+
+    // --------------------------------------------------//
+
+    // Os getters e o construtor acima podem ser escritos da seguinte forma:
+    constructor(
+        private _data: Date,
+        public readonly quantidade: number,
+        public readonly valor: number
+    ) {}
+    // O readonly só não deixa trabalhar com atribuição, os valores ainda podem ser modificados
+    // com método invocados neles. Ex. negociacao.data.setDate();
 
     get data(): Date {
-        return this._data;
+        const data = new Date(this._data.getTime());
+        return data;
     }
 
-    get quantidade(): number {
-        return this._quantidade;
-    }
-
-    get valor(): number {
-        return this._valor;
-    }
-
+    // --------------------------------------------------//
     get volume(): number {
-        return this._quantidade * this.valor;
+        return this.quantidade * this.valor;
     }
 }
